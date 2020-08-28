0d9a76d14d719d875a314bdfe259fd0a1c21e37b
Kris-os <kristopher.osullivan@gmail.com>
2020-08-28 19:31:02 +0100

    Updates

---
+++ precache-manifest.c235b8e76953602797e849f77c86658b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a61e4d7a64bd76fc1598acc30896cf",
+    "url": "/fire/index.html"
+  },
+  {
+    "revision": "d4802ff39c24c968306b",
+    "url": "/fire/static/css/main.ece6b6ec.chunk.css"
+  },
+  {
+    "revision": "46d7b50986cd1e71f586",
+    "url": "/fire/static/js/2.65c860db.chunk.js"
+  },
+  {
+    "revision": "d4802ff39c24c968306b",
+    "url": "/fire/static/js/main.cde66f7c.chunk.js"
+  },
+  {
+    "revision": "dae024668cd9b2ab33ad",
+    "url": "/fire/static/js/runtime-main.0b06fc89.js"
+  }
+]);
