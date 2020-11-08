06a44cc3afeea1455757a8e8442506af46814749
Kris-os <kristopher.osullivan@gmail.com>
2020-11-08 17:51:40 +0000

    Add % HLE results. Convert some components to functional from class

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Inputs from "./components/Inputs";
 import Header from "./components/Header";
 import NumberInput from "./components/NumberInput";
 import ResultsSummary from "./components/ResultsSummary";
+import Intro from "./components/Intro";
 import { Card, CardDeck, Col, Container, Row } from "react-bootstrap";
 import "./custom.scss";
 import "./App.css";
@@ -36,8 +37,10 @@ class App extends Component {
         yearsMonthsDays3: {},
         yearsMonthsDays4: {},
         lifeExpectancyResults: {
-          lifeExpectancy: [],
-          healthyLifeExpectancy: [],
+          lifeExpectancyYearsLeft: [],
+          healthyLifeExpectancyYearsLeft: [],
+          lifeExpectancyAge: [],
+          healthyLifeExpectancyAge: [],
         },
       },
     };
@@ -57,9 +60,10 @@ class App extends Component {
   };
 
   runCalcs() {
-    //const baseUrl = "https://opulazurefunction.azurewebsites.net/api/OpulFunction?code=";
-    const url = "http://localhost:7071/api/OpulFunction";
-    //const url = baseUrl.concat(process.env.REACT_APP_API_KEY);
+    const baseUrl =
+      "https://opulazurefunction.azurewebsites.net/api/OpulFunction?code=";
+    //const url = "http://localhost:7071/api/OpulFunction";
+    const url = baseUrl.concat(process.env.REACT_APP_API_KEY);
 
     const response = fetch(url, {
       method: "POST",
@@ -82,45 +86,7 @@ class App extends Component {
         <Container fluid>
           <Row className="align-items-start">
             <Col md="4" className="padding30">
-              <h4>
-                <div className="bold">What is Financial Freedom?</div>
-              </h4>
-              <h5>
-                <br />A state of freedom where you can spend your time as you
-                wish, free from the need to work for money. Waking up each day
-                and deciding how you want to live it. The only requirement -
-                enough financial wealth to support your desired lifestyle.
-                <br />
-                <br />
-                The default age in society for financial freedom is after we
-                have used up most of our health and time, aged 65 to 70. This is
-                when our Governments provide pensions.{" "}
-                <text className="bold">
-                  However, it is possible to reach financial freedom much sooner
-                  than this.{" "}
-                </text>
-                Across most of the world, 10 years of work is enough to keep you
-                sheltered, fed and happy for the rest of your days.
-                <br />
-                <br />
-                You have a choice how simple or luxurious your lifestyle across
-                your lifetime is. In fact, this is the{" "}
-                <text className="bold">same choice</text> as the number of years
-                you work across your lifetime. This is shown in the [Lifestyle]
-                section.
-                <br />
-                <br />
-                The Opul toolkit helps you make this decision and answer other
-                questions such as:
-                <br />
-                <div className="italic">
-                  How much wealth do I need?
-                  <br />
-                  How soon can I reach Financial Freedom? <br />
-                  What is the trade-off between lifestyle and years worked?
-                </div>
-              </h5>
-
+              <Intro />
               <Inputs
                 className="paddingTopAndBottomLarge"
                 updateState={this.updateState}
@@ -130,11 +96,18 @@ class App extends Component {
             <Col md="8" className="padding30">
               <ResultsSummary
                 age={this.state.inputsDictionary.age}
-                lifeExpectancy={
-                  this.state.results.lifeExpectancyResults.lifeExpectancy
+                lifeExpectancyResults={this.state.results.lifeExpectancyResults}
+                baseCasePercentHleWorking={
+                  this.state.results.baseCasePercentHleWorking
                 }
-                healthyLifeExpectancy={
-                  this.state.results.lifeExpectancyResults.healthyLifeExpectancy
+                baseCasePercentHleFree={
+                  this.state.results.baseCasePercentHleFree
+                }
+                lowerMonthlySpendPercentHleWorking={
+                  this.state.results.lowerMonthlySpendPercentHleWorking
+                }
+                lowerMonthlySpendPercentHleFree={
+                  this.state.results.lowerMonthlySpendPercentHleFree
                 }
               />
               <h3 className="paddingTopAndBottomLarge">
--- src/components/ResultsSummary.jsx
+++ src/components/ResultsSummary.jsx
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 
-class ResultsSummary extends Component {
-  render() {
-    return (
+function ResultsSummary (props) {
+      return (
       <div>
         {" "}
         <div>
-          You are <text className="bold">{this.props.age}</text> years old{" "}
+          You are <text className="bold">{props.age}</text> years old{" "}
         </div>
         <div>
-          Your life expectancy is{" "}
-          <text className="bold">{this.props.lifeExpectancy}</text> years{" "}
+          Your life expectancy is age{" "}
+          <text className="bold">{props.lifeExpectancyResults.lifeExpectancyAge}</text>{" "}
+          (in <text className="bold">{props.lifeExpectancyResults.lifeExpectancyYearsLeft}</text> years)
         </div>
         <div>
-          Your healthy life expectancy is{" "}
-          <text className="bold">{this.props.healthyLifeExpectancy}</text> years{" "}
+          Your healthy life expectancy is age{" "}
+          <text className="bold">{props.lifeExpectancyResults.healthyLifeExpectancyAge}</text>{" "}
+          (in <text className="bold">{props.lifeExpectancyResults.healthyLifeExpectancyYearsLeft}</text> years)
         </div>
         <div>
           Under your base case inputs, you spend{" "}
-          <text className="bold">62%</text> of your healthy life expectancy
-          working, and <text className="bold">28%</text> financially free.
+          <text className="bold">{props.baseCasePercentHleWorking}%</text> of your healthy life expectancy
+          working, and <text className="bold">{props.baseCasePercentHleFree}%</text> financially free.
         </div>
         <div>
           Under the reduced expenditure scenario, you spend{" "}
-          <text className="bold">52%</text> of your healthy life expectancy
-          working, and <text className="bold">48%</text> financially free.
+          <text className="bold">{props.lowerMonthlySpendPercentHleWorking}%</text> of your healthy life expectancy
+          working, and <text className="bold">{props.lowerMonthlySpendPercentHleFree}%</text> financially free.
         </div>
       </div>
     );
-  }
 }
 
 export default ResultsSummary;
+
+
