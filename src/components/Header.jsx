e5ccd856153accf802bd5709097b1b2be37b83f4
Kris-os <kristopher.osullivan@gmail.com>
2020-11-11 17:27:27 +0000

    NavBar some improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,34 +7,32 @@ class Header extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <Navbar bg="dark">
-          <Navbar.Brand>
-            <Nav.Link as={Link} to="/">
-              <img
-                alt=""
-                src={OpulIcon}
-                width="140"
-                height="70"
-                className="d-inline-block align-top"
-              />
-            </Nav.Link>
-          </Navbar.Brand>
-          {/* //<Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-          <Navbar.Collapse id="basic-navbar-nav">
-            <div>&nbsp;&nbsp;The Financial Freedom toolkit</div>
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand>
+          <Nav.Link as={Link} to="/">
+            <img
+              alt=""
+              src={OpulIcon}
+              width="140"
+              height="70"
+              className="d-inline-block align-top"
+            />
+          </Nav.Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* <Navbar.Text>&nbsp;&nbsp;The Financial Freedom toolkit</Navbar.Text> */}
 
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/model">
-                &nbsp;&nbsp;&nbsp;Model&nbsp;
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about">
-                &nbsp;&nbsp;&nbsp;About&nbsp;
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/model">
+              &nbsp;&nbsp;&nbsp;Model&nbsp;
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              &nbsp;&nbsp;&nbsp;About&nbsp;
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
