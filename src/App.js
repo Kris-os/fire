5e3584775d5766c98986addff23000bc5e458fd3
Kris-os <kristopher.osullivan@gmail.com>
2020-11-11 15:10:02 +0000

    Add Util function file. Convert App to Functional Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Model from "./components/Model";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
@@ -6,31 +6,18 @@ import "./custom.scss";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route path="/" exact component={Intro} />
-            <Route path="/model" component={Model} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
-
-function encodeQueryData(data) {
-  const output = [];
-  for (let d in data)
-    output.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-  return output.join("&");
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/intro" exact component={Intro} />
+          <Route path="/model" component={Model} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Util.js
@@ -0,0 +1,6 @@
+function encodeQueryData(data) {
+  const output = [];
+  for (let d in data)
+    output.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+  return output.join("&");
+}
