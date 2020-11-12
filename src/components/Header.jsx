bd9e9ef5521c133ee6c200a8db7ff3588cde6fee
Kris-os <kristopher.osullivan@gmail.com>
2020-11-12 16:48:58 +0000

    Convert to function components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,40 +3,37 @@ import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import OpulIcon from "../Images/OpulIcon.png";
 
-class Header extends Component {
-  state = {};
-  render() {
-    return (
-      <Navbar bg="dark" expand="lg">
-        <Navbar.Brand>
-          <Nav.Link as={Link} to="/">
-            <img
-              alt=""
-              src={OpulIcon}
-              width="140"
-              height="70"
-              className="d-inline-block align-top"
-            />
-          </Nav.Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Navbar.Text style={{ color: "black" }}>
-            &nbsp;&nbsp;The Financial Freedom toolkit
-          </Navbar.Text>
+function Header() {
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Navbar.Brand>
+        <Nav.Link as={Link} to="/">
+          <img
+            alt=""
+            src={OpulIcon}
+            width="140"
+            height="70"
+            className="d-inline-block align-top"
+          />
+        </Nav.Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Text style={{ color: "black" }}>
+          &nbsp;&nbsp;The Financial Freedom toolkit
+        </Navbar.Text>
 
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/model">
-              &nbsp;&nbsp;&nbsp;Model&nbsp;
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about">
-              &nbsp;&nbsp;&nbsp;About&nbsp;
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/model">
+            &nbsp;&nbsp;&nbsp;Model&nbsp;
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            &nbsp;&nbsp;&nbsp;About&nbsp;
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default Header;
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -2,104 +2,102 @@ import React, { Component } from "react";
 import { Form, Col, Row } from "react-bootstrap";
 import NumberInput from "./NumberInput";
 
-class Inputs extends Component {
-  handleSubmit = (event) => {
+function Inputs(props) {
+  const handleSubmit = (event) => {
     event.preventDefault();
   };
 
-  render() {
-    return (
-      <div className={"inputs " + this.props.className}>
-        <h3>Inputs</h3>
-        <br></br>
-        <Form>
-          <Row className="paddingTopAndBottomSmall">
-            <Col column sm={6}>
-              Age
-            </Col>
-            <Col sm={6}>
-              <NumberInput
-                id="age"
-                updateState={this.props.updateState}
-                min={0}
-                max={89}
-              />
-            </Col>
-          </Row>
+  return (
+    <div className={"inputs " + props.className}>
+      <h3>Inputs</h3>
+      <br></br>
+      <Form>
+        <Row className="paddingTopAndBottomSmall">
+          <Col column sm={6}>
+            Age
+          </Col>
+          <Col sm={6}>
+            <NumberInput
+              id="age"
+              updateState={props.updateState}
+              min={0}
+              max={89}
+            />
+          </Col>
+        </Row>
 
-          <Row className="paddingTopAndBottomSmall">
-            <Col column sm={6}>
-              Net-worth
-            </Col>
-            <Col sm={6}>
-              <NumberInput id="networth" updateState={this.props.updateState} />
-            </Col>
-          </Row>
+        <Row className="paddingTopAndBottomSmall">
+          <Col column sm={6}>
+            Net-worth
+          </Col>
+          <Col sm={6}>
+            <NumberInput id="networth" updateState={props.updateState} />
+          </Col>
+        </Row>
 
-          <Row className="paddingTopAndBottomSmall">
-            <Col column sm={6}>
-              Annual net earnings
-            </Col>
-            <Col sm={6}>
-              <NumberInput
-                id="netEarnings"
-                updateState={this.props.updateState}
-                min={0}
-              />
-            </Col>
-          </Row>
+        <Row className="paddingTopAndBottomSmall">
+          <Col column sm={6}>
+            Annual net earnings
+          </Col>
+          <Col sm={6}>
+            <NumberInput
+              id="netEarnings"
+              updateState={props.updateState}
+              min={0}
+            />
+          </Col>
+        </Row>
 
-          <Row className="paddingTopAndBottomSmall">
-            <Col column sm={6}>
-              Annual cost of lifestyle
-            </Col>
-            <Col sm={6}>
-              <NumberInput
-                id="expenditure"
-                updateState={this.props.updateState}
-                min={0}
-              />
-            </Col>
-          </Row>
+        <Row className="paddingTopAndBottomSmall">
+          <Col column sm={6}>
+            Annual cost of lifestyle
+          </Col>
+          <Col sm={6}>
+            <NumberInput
+              id="expenditure"
+              updateState={props.updateState}
+              min={0}
+            />
+          </Col>
+        </Row>
 
-          <Row className="paddingTopAndBottomSmall">
-            <Col column sm={6}>
-              Inheritance to leave behind
-            </Col>
-            <Col sm={6}>
-              <NumberInput
-                id="inheritanceToLeaveBehind"
-                updateState={this.props.updateState}
-                min={0}
-              />
-            </Col>
-          </Row>
-        </Form>
+        <Row className="paddingTopAndBottomSmall">
+          <Col column sm={6}>
+            Inheritance to leave behind
+          </Col>
+          <Col sm={6}>
+            <NumberInput
+              id="inheritanceToLeaveBehind"
+              updateState={props.updateState}
+              min={0}
+            />
+          </Col>
+        </Row>
+      </Form>
 
-        <br></br>
-        <h3>Assumptions</h3>
-        <br></br>
-        <Form onSubmit={this.handleSubmit}>
-          <Row className="paddingTopAndBottomSmall">
-            <Col column sm={6}>
-              Investment return above inflation (% pa)
-            </Col>
-            <Col sm={6}>
-              <NumberInput
-                id="returnAboveInflationAssumption"
-                updateState={this.props.updateState}
-                min={0}
-                max={8}
-                precision={1}
-                step={0.1}
-                initialValue={4}
-              />
-            </Col>
-          </Row>
-        </Form>
-      </div>
-    );
-  }
+      <br></br>
+      <h3>Assumptions</h3>
+      <br></br>
+      <Form onSubmit={handleSubmit}>
+        <Row className="paddingTopAndBottomSmall">
+          <Col column sm={6}>
+            Investment return above inflation (% pa)
+          </Col>
+          <Col sm={6}>
+            <NumberInput
+              id="returnAboveInflationAssumption"
+              updateState={props.updateState}
+              min={0}
+              max={8}
+              precision={1}
+              step={0.1}
+              initialValue={4}
+            />
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
 }
 
 export default Inputs;
